c1d704daa3f3846fc23d19abeaeb00941e1d78e6
Marjorie <marjoriemiddle@gmail.com>
2024-08-09 15:25:05 -0400

    History.html

---
+++ navbar.js
@@ -0,0 +1,21 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo')
+
+//Mobile Menu
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+function Home() {
+    window.location.href = "income.html";
+}
+function incomeHistory() {
+    window.location.href = "History.html";
+
+    
+}
+
+
+
